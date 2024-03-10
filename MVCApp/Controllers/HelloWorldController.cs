8311a8d09a4e4efe592fdb1ff8b961ffe3709677
GuillermoStrong <luisguillermofuerte@outlook.com>
2024-03-10 02:38:42 -0600

    Add project files.

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["numTimes"] = numTimes;
+            return View();
+        }
+
+        //Get: /HelloWorld/
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        //Get: /HelloWorld/Welcome/
+        //Requieres using System.Text.Encoding.Web
+        //Get: /HelloWorld/Welcome?name=guillermo&numtimes=5/
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    //return "This is action method...";
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+    }
+}
